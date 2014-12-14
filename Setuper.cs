ajhyndman/heroesdb-be

commit 6740d8297a1a875ff949331120388630320c8b8b

Performance improvement.

Setuper.cs
@@ -625,10 +625,11 @@ namespace HeroesDB {
 				";
 				var reader = command.ExecuteReader();
 				var missingIcons = new List<String>();
+				var files = new List<String>(Directory.GetFiles(iconPath));
 				while (reader.Read()) {
 					var iconFileName = String.Concat(reader["Icon"], ".tga");
 					var iconFile = Path.Combine(iconPath, iconFileName);
-					if (!File.Exists(iconFile)) {
+					if (files.Contains(iconFile)) {
 						missingIcons.Add(Convert.ToString(reader["Icon"]));
 					}
 				}

commit 9dbbe774dea1026e4a3a5484f8854a64140d482c

Fixing 6740d8297a introduced bug.

Setuper.cs
@@ -601,11 +601,10 @@ namespace HeroesDB {
 				";
 				var reader = command.ExecuteReader();
 				var missingIcons = new List<String>();
-				var files = new List<String>(Directory.GetFiles(iconPath));
 				while (reader.Read()) {
 					var iconFileName = String.Concat(reader["Icon"], ".tga");
 					var iconFile = Path.Combine(iconPath, iconFileName);
-					if (files.Contains(iconFile)) {
+					if (!File.Exists(iconFile)) {
 						missingIcons.Add(Convert.ToString(reader["Icon"]));
 					}
 				}